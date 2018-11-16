9b5cb773645915db1d07fdfee461d9f5e53b93f9
Hendra Anggrian <hendraanggrian@gmail.com>
2018-11-17 00:17:24 +0700

    Reader no longer serializable

--- r-gradle-plugin/src/com/hendraanggrian/generating/r/reader/Reader.kt
+++ r-gradle-plugin/src/com/hendraanggrian/generating/r/reader/Reader.kt
@@ -2,9 +2,8 @@ package com.hendraanggrian.generating.r.reader
 
 import com.squareup.javapoet.TypeSpec
 import java.io.File
-import java.io.Serializable
 
-internal interface Reader : Serializable {
+internal interface Reader {
 
     fun read(typeBuilder: TypeSpec.Builder, file: File): Boolean
 }
