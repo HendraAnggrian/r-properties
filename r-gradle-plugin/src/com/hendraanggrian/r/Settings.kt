1517d97c581449657018c0b1b80b84725545287e
Hendra Anggrian <hendraanggrian@gmail.com>
2020-01-17 20:27:41 +0700

    Use `@param` when documenting `Settings.kt`

--- r-gradle-plugin/src/com/hendraanggrian/r/Settings.kt
+++ r-gradle-plugin/src/com/hendraanggrian/r/Settings.kt
@@ -7,37 +7,18 @@ import java.nio.charset.StandardCharsets
 /**
  * Settings for customizing the field generation of CSS file.
  *
+ * @param charset charset to be used in case neither a charset rule nor a BOM is present, default is [StandardCharsets.UTF_8].
+ * @param cssVersion version to use when parsing CSS file, default is [ECSSVersion.CSS30].
+ * @param isWriteElementTypeSelector determine whether adapter should write element type selector, default is false.
+ * @param isWriteClassSelector determine whether adapter should write class selector, default is true.
+ * @param isWriteIdSelector determine whether adapter should write ID selector, default is true.
  * @see RTask.configureCss
  */
 data class CssSettings(
-    /**
-     * The charset to be used in case neither a [charset] rule nor a BOM is present.
-     * Default value is [StandardCharsets.UTF_8].
-     */
     var charset: Charset = StandardCharsets.UTF_8,
-
-    /**
-     * The CSS version to use.
-     * Default value is [ECSSVersion.CSS30].
-     */
     var cssVersion: ECSSVersion = ECSSVersion.CSS30,
-
-    /**
-     * Determine whether adapter should write element type selector (e.g.: `ul { ... }`)
-     * It is disabled by default.
-     */
     var isWriteElementTypeSelector: Boolean = false,
-
-    /**
-     * Determine whether adapter should write class selector (e.g.: `.box { ... }`)
-     * It is enabled by default.
-     */
     var isWriteClassSelector: Boolean = true,
-
-    /**
-     * Determine whether adapter should write ID selector (e.g.: `#container { ... }`)
-     * It is enabled by default.
-     */
     var isWriteIdSelector: Boolean = true
 ) {
 
@@ -70,19 +51,12 @@ data class CssSettings(
 /**
  * Settings for customizing the field generation of json file.
  *
+ * @param isRecursive determine whether adapter should also write inner json object, default is false.
+ * @param isWriteArray extended property of [isRecursive] that will also write inner json array, default is true.
  * @see RTask.configureJson
  */
 data class JsonSettings(
-    /**
-     * Determine whether adapter should also write inner json object.
-     * It is disabled by default.
-     */
     var isRecursive: Boolean = false,
-
-    /**
-     * Determine whether adapter should also write inner json array.
-     * It is enabled by default. However, has no effect if [isRecursive] is disabled.
-     */
     var isWriteArray: Boolean = true
 ) {
 
@@ -100,14 +74,10 @@ data class JsonSettings(
 /**
  * Settings for customizing the field generation of properties file.
  *
+ * @param isWriteResourceBundle determine whether adapter should also write resource bundle, default is false.
  * @see RTask.configureProperties
  */
 data class PropertiesSettings(
-    /**
-     * Determine whether adapter should also write resource bundle.
-     * Resource bundle files usually have suffix like `_en`, `_id`.
-     * It is disabled by default.
-     */
     var isWriteResourceBundle: Boolean = false
 ) {
 
